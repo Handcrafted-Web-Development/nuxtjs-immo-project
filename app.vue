0674c4cd6dcfb73a89e5a5bf9e7de54da3912577
Lucas-Debiais <lucas.debiais11@gmail.com>
2024-02-19 14:57:09 +0100

    WIP: ECV-25 -> init header & footer

--- app.vue
+++ app.vue
@@ -1,3 +1,5 @@
 <template>
+  <Header/>
   <NuxtPage/>
+  <Footer/>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="text-red-500">
-    test
-  </div>
+
 </template>
 <script setup lang="ts">
 </script>
